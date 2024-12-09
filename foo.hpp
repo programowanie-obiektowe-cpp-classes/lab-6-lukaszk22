1755b33a045d9c75a8cc1b30f75b47b61a98f263
lukaszk22 <lukaszkicinski22@gmail.com>
2024-12-09 14:12:01 +0100

    Zejściówka nr 6

--- foo.hpp
+++ foo.hpp
@@ -6,11 +6,21 @@
 #include <list>
 #include <vector>
 
+char uOperation(const Human& h)
+{
+    if(h.isMonster())
+        return 'n';
+    else    
+        return 'y';
+}
 std::vector< char > foo(std::list< Human >& people)
 {
     std::vector< char > retval(people.size());
 
     // Twoja implementacja tutaj
+    std::for_each(people.begin(), people.end(), [](Human& h){h.birthday();});
+
+    std::transform(people.rbegin(), people.rend(), retval.begin(), uOperation);
 
     return retval;
 }
